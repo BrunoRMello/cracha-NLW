c7e33cf473c2d001a259736e401f275af9f1626a
Bruno Mello <59987398+BrunoRMello@users.noreply.github.com>
2021-10-22 13:15:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const links = {
+  github: "BrunoRMello",
+  youtube: "brunomello",
+  facebook: "BrunoMello",
+  instagram: "@brvolk",
+  twitter: "BrunoVolk",
+};
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute("class");
+    li.children[0].href = `https://${social}.com/${links[social]}`;
+  }
+}
+
+changeSocialMediaLinks();
+
+function getGitHubProfile() {
+  const url = `https://api.github.com/users/${links.github}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      userName.textContent = data.name;
+      userBio.textContent = data.bio;
+      userLink.href = data.html_url;
+      userPhoto.src = data.avatar_url;
+      userLogin.textContent = data.login;
+    });
+}
+getGitHubProfile();
